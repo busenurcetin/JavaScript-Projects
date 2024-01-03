5158a9fc9a2686f91491b3e44443f3b7b1497356
Buse Nur <110244548+busenurcetin@users.noreply.github.com>
2024-01-03 18:04:33 +0300

    Add files via upload

---
+++ JavaScript-Project5-Sidebar/app.js
@@ -0,0 +1,18 @@
+const toogleBtn = document.querySelector(".sidebar-toggle");
+const closeBtn = document.querySelector(".close-btn");
+const sidebar = document.querySelector(".sidebar");
+
+toogleBtn.addEventListener("click", function () { 
+  // if(sidebar.classList.contains("show-sidebar")){
+  //   sidebar.classList.remove("show-sidebar");
+  // }
+  // else{
+  //   sidebar.classList.add("show-sidebar");
+  // }
+
+  sidebar.classList.toggle("show-sidebar");
+});
+
+closeBtn.addEventListener("click", function () {
+  sidebar.classList.remove("show-sidebar");
+});
